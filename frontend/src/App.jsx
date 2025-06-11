db4dca668b56d600e1f7f7ca4682f4547596b1fe
Mlowegene <genemlowe@gmail.com>
2025-06-11 16:11:24 +0300

    feat: added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,33 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Root from "./components/root";
+import Root from "./utils/root";
 import Login from "./pages/login";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Root />} />
-        <Route path="/admin/dashboard" element={<h1>admin dashboard</h1>} />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoutes requiredRole={["admin"]}>
+              <h1>admin dashboard</h1>
+            </ProtectedRoutes>
+          }
+        />
         <Route
           path="/customer/dashboard"
           element={<h1>Customer dashboard</h1>}
         />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/unauthorized"
+          element={
+            <h1 className="font-bold text-red-500 mt-20 ">Unauthorized User</h1>
+          }
+        />
       </Routes>
     </Router>
   );
