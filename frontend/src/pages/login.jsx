9f08e91e0a804a82a8aea8627af90b3d2df6751f
Mlowegene <genemlowe@gmail.com>
2025-06-11 15:25:03 +0300

    feat: added login functionality

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,18 +1,66 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import { useAuth } from "../context/AuthContext";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const { login } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/api/auth/login`,
+        {
+          email,
+          password,
+        }
+      );
+
+      if(response.data.sucess) {
+
+        await login(response.data.user, response.data.token);
+        if (response.data.user.role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/customer/dashboard");
+        }
+      } else {
+        alert(response.data.error);
+      }
+    } catch (error) {
+      setError(error.response?.data?.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-green-600 from-50% to-gray-100 to-50% space-y-6">
       <h2 className="text-3xl text-white">Inventory Management System</h2>
       <div className="border shadow-lg p-6 w-120 bg-white">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form>
+        {error && (
+          <div className="bg-red-200 text-red-700 p-2 mb-4 rounded">
+            {error}
+          </div>
+        )}
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700">Email</label>
             <input
               className="w-full px-3 py-2 border"
               type="email"
               name="email"
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="enter your email"
               required
             />
@@ -25,6 +73,7 @@ function Login() {
               className="w-full px-3 py-2 border"
               type="password"
               name="password"
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="enter your password"
               required
             />
@@ -34,7 +83,7 @@ function Login() {
               className="w-full bg-green-600 text-white py-2"
               type="submit"
             >
-              Login
+              {loading ? "Loading..." : "Login"}
             </button>
           </div>
         </form>
