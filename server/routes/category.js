75501d906742cfedcc6c0b8e664f844f241199aa
Mlowegene <genemlowe@gmail.com>
2025-06-16 11:47:27 +0300

    feat: added middleware

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { addCategory } from "../controller/categoryController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/add", addCategory);
+router.post("/add", authMiddleware, addCategory);
 
 export default router;
